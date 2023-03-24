0995b4353ac72d5118ab87c39181a90acf15e5c8
Doshijainam <Jdoshi3117@conestogac.on.ca>
2023-03-23 23:33:47 -0400

    Faculty class Implemented

--- group13UniBuddy/Faculty.cs
+++ group13UniBuddy/Faculty.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,66 @@ using System.Threading.Tasks;
 
 namespace group13UniBuddy
 {
-    internal class Faculty
+
+    public class Faculty
     {
+        string Name { get; set; }
+
+        public List<Course> Courses { get; set; }
+
+        public Faculty(string name)
+        {
+            Name = name;
+            Courses = new List<Course>();
+        }
+
+        public void AddCourse(Course course)
+        {
+            Courses.Add(course);
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            Courses.Remove(course);
+        }
+    }
+    public class FacultyList:IEnumerable<Faculty>
+    {
+
+        private List<Faculty> faculty = new List<Faculty>();
+
+       public void Add(Faculty fac)
+        {
+            faculty.Add(fac);
+        }
+
+        public void Remove(Faculty fac)
+        {
+            faculty.Remove(fac);
+        }
+
+        
+
+        public IEnumerator<Faculty> GetEnumerator()
+        {
+            return faculty.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
+
+public class Course
+{
+    public string Name { get; set; }
+   
+
+    public Course(string name)
+    {
+        Name = name;
     }
 }
