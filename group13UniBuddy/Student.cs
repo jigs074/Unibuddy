e5394f37e523a248a9c23c38f42ddc6504536404
jigs074 <90673637+jigs074@users.noreply.github.com>
2023-03-23 23:29:36 -0400

    Student class implemented

--- group13UniBuddy/Student.cs
+++ group13UniBuddy/Student.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections; 
 
 namespace group13UniBuddy
 {
-    public class Student
-    {
-        string name; 
-        int age;
-        string Category; 
+   
+       
+    
+        public class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Category { get; set; }
+            public string Program { get; set; }
+            public string StudentNo { get; set; }
+        }
 
+        public class StudentList : IEnumerable<Student>
+        {
+            private List<Student> _students = new List<Student>();
+
+            public void Add(Student student)
+            {
+                _students.Add(student);
+            }
+
+            public IEnumerator<Student> GetEnumerator()
+            {
+                return _students.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
     }
-}
+
+
+
