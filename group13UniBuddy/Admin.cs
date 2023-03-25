14d027ccf7593a89b7d83b2780b67498d7cecdec
Doshijainam <Jdoshi3117@conestogac.on.ca>
2023-03-25 14:26:34 -0400

    Admin Class Implemented!

--- group13UniBuddy/Admin.cs
+++ group13UniBuddy/Admin.cs
@@ -7,7 +7,31 @@ using System.Threading.Tasks;
 namespace group13UniBuddy
 {
     public class Admin
-    { 
+    {
+        private int i = 0;
+        public string ID = "Admin";
+        public string password = "Admin";
 
+        public void giveAccess()
+        {
+            for(i = 0;i<3;i++)
+            {
+                Console.WriteLine("Enter Id:");
+                string id = Console.ReadLine();
+                Console.WriteLine("Enter Password:");
+                string pass = Console.ReadLine();
+
+                if(ID==id && password == pass)
+                {
+                    Console.WriteLine("Admin Access Granted!");
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
     }
 }
+
+    
